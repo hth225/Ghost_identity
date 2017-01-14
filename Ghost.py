5ef82ade231219a82b690bd06829c39744d6af50
Taeho Hwang <hth225@naver.com>
2017-01-14 23:24:54 +0900

    add ghost file save function

--- Ghost.py
+++ Ghost.py
@@ -8,12 +8,25 @@ def Various_identity():
     for _ in range(0, 10):
         print(fake.simple_profile())
 
+def write_identities(cache):
+    file = open("Got_Identities.txt", 'a')
+    file.write(str(cache))
+    file.close()
+
+
 if __name__ == "__main__":
     while 1:
         print("1 : Single identity\n2 : Various identity\n3 : Exit\n")
         value = input(">")
         if (value == '1'):
             Get_person_identity()
+            cache = Get_person_identity()
+            q = input("Do you want to write this on text file? (y/n)\n")
+            if(q == 'y'):
+                write_identities(str(cache))
+                print("Successfully done")
+            else:
+                break
         elif(value == '2'):
             print("Make 10person identity")
             Various_identity()
