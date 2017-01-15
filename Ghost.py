c82b6465a1653c8dd942c6176534bcc34ba3d436
Taeho Hwang <hth225@naver.com>
2017-01-15 22:11:45 +0900

    file flush function added

--- Ghost.py
+++ Ghost.py
@@ -21,9 +21,17 @@ def write_identities(): #writing single identities on Got_Identities.txt
     file.write("\n")
     file.close()
 
+def file_flush(): #clear all information that wrote
+    file = open("various.txt", "w")
+    file_2nd = open("Got_Identities.txt", "w")
+    file.write("")
+    file.close()
+    file_2nd.write("")
+    file_2nd.close()
+
 if __name__ == "__main__":
     while 1:
-        print("1 : Single identity\n2 : Various identity\n3 : Exit\n")
+        print("1 : Single identity\n2 : Various identity\n3 : File flush\n4 : Exit")
         value = input(">")
         if (value == '1'): #get one identity
             Get_person_identity()
@@ -31,7 +39,7 @@ if __name__ == "__main__":
             if(q == 'y'): #writing on text file
                 write_identities()
                 print("Successfully done\n")
-            else: #if user typed n or another charachter, break loop
+            else: #if user typed n or another charachter, escape loop
                 break
         elif(value == '2'): #get various identities for 10 person
             print("Make 10person identity\n")
@@ -41,8 +49,11 @@ if __name__ == "__main__":
             if(qe == 'y'): #writing on text file
                 write_Various_identities()
                 print("Successfully done\n")
-            else: #if user typed n or another charachter, break loop
+            else: #if user typed n or another charachter, escape loop
                 break
+        elif(value == '3'):
+            print("file flush process successfully done")
+            file_flush()
         else: #exit
             print("exit")
             break
